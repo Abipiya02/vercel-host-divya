e64cfb46d6983792012c89dc889f28d0b7c51736
Abipiya02 <atchayaangusamy@gmail.com>
2024-11-07 08:40:17 -0800

    initial commit2

--- Frontend/login-app/src/login.jsx
+++ Frontend/login-app/src/login.jsx
@@ -12,7 +12,7 @@ function Login() {
     const handleSubmit=(e)=>{
         const form=document.querySelector('form')
         e.preventDefault()
-        axios.post('http://localhost:3001/login',{email,password})
+        axios.post('https://vercel-host-divya.vercel.app//login',{email,password})
         .then(result=>{console.log(result)
         if(result.data==='success'){
           navigate('/')
